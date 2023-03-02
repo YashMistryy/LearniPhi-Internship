bba470020869afd9456a815eb78f4dc9efffcc23
yash mistry <mistryy37@gmail.com>
2023-03-02 10:51:41 +0530

    ..

---
+++ Python Practice/datatypes.py
@@ -0,0 +1,12 @@
+# following are the datatypes as well as data structure in python
+# dataTypes are ,
+
+# strings
+# --> strings are immutable sequence of characters 
+# -->python does not have a character datatype so 's' will be considered as a string of length 1
+# --> strings can be declared like 'a string ex' or "this is also a string"
+
+str1 = "this is my string"
+print(str1)
+str2 = "".join(reversed(str1))
+print(str2)
